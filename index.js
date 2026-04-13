ce1f79f02f03fcd3899506c357e1dcf96994cccc
Mario Usanga <mariobomausanga@gmail.com>
2026-04-13 18:55:12 +1000

    Switching to manual CORS headers for bot compatibility

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-
-app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
 
 app.get('/check-gender/:name', async (req, res) => {
     try{
